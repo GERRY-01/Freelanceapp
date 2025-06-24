3a2ae6da8494d760a9f1d0768f733b28b7370ce5
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-24 15:16:44 +0300

    modified the model for customuser

--- jobapp/models.py
+++ jobapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class CustomUser(models.Model):
-    user = models.oneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=200)
     ROLE = (
         ('freelancer', 'freelancer'),

a44dcf099db5cfe6324e78d859f00ca87f23b925
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-24 16:56:39 +0300

    added the path for the clients page

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
-    path('jobs', views.jobs, name='jobs'),
+    path('freelancer', views.freelancer, name='freelancer'),
+    path('client', views.client, name='client'),
 ]
